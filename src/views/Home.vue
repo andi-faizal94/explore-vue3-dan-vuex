e292a6076e7bf7c190765091b96d0f47072af5c2
andi-faizal94 <andifaizal647@gmail.com>
2021-03-21 22:30:03 +0700

    mengexplore vue 3 dan vuex

---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="counter">{{name.counter}}</div>
+    <p>apakah ini benar?</p>
+    <p>{{nameCounter}}</p>
+
+    <div class="button">
+      <button @click="increase">+</button>
+      <button @click="decrease">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      name: {
+        counter: 0
+      }
+    };
+  },
+
+  computed: {
+    nameCounter() {
+      return this.name.counter.length > 1 ? "yes" : "no";
+    }
+  },
+  methods: {
+    increase() {
+      this.name.counter++;
+    },
+    decrease() {
+      this.name.counter--;
+    }
+  }
+};
+</script>
+
+
+<style >
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 50px;
+}
+.button button {
+  font-size: 50px;
+  width: 50px;
+  margin: 0 10px;
+}
+</style> 
